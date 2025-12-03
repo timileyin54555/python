58c43d7334e7a4bac65d41279378321f2ec1412e
timileyin54555 <ashirutimileyin33@gmail.com>
2025-12-03 11:44:39 +0100

    Add files via upload

---
+++ safe.py
@@ -0,0 +1,10 @@
+first_number = int(input("Enter first number:"))
+
+second_number = int(input("Enter second number:"))
+
+answer = (first_number / second_number)
+
+if(second_number != 0):
+    print("cannot be divided by zero")
+elif(second_number == 0):
+    print("Find another number")
